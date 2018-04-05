17f3436cfd8a47150f09e648da349c414eccf227
Дима Старков <dstarkdev@gmail.com>
2018-04-05 11:05:12 +0500

    To strings

--- task-3/numbers.c
+++ task-3/numbers.c
@@ -8,19 +8,19 @@
 #define BUFFER_SIZE 1024
 
 typedef struct node {
-    long val;
+    char* val;
     struct node* next;
 } node_t;
 
 node_t* root = NULL;
 
-void init(node_t** node, long val) {
+void init(node_t** node, char* val) {
     (*node) = malloc(sizeof(node_t));
     (*node)->val = val;
     (*node)->next = NULL;
 }
 
-void push(long val) {
+void push(char* val) {
     if (root == NULL) {
         init(&root, val);
     } else {
@@ -45,37 +45,99 @@ long size() {
     return count;
 }
 
-int cmpnums(const void* a, const void* b) {
-    return *(long*)a - *(long*) b;
+char* concat(const char* s1, const char* s2) {
+    char* result = malloc(strlen(s1) + strlen(s2) + 1);
+
+    strcpy(result, s1);
+    strcat(result, s2);
+
+    return result;
+}
+
+char* substring(char* string, int position, int length) {
+    char* pointer = malloc(length + 1);
+   
+    for (int c = 0; c < position - 1 ; c++)
+        string++; 
+ 
+    for (int c = 0; c < length; c++) {
+        *(pointer + c) = *string;      
+        string++;
+    }
+ 
+    *(pointer + length) = '\0';
+ 
+    return pointer;
+}
+
+int cmp_str_as_number(const void* a, const void* b) {
+    char* ac = *(char**)a;
+    char* bc = *(char**)b;
+
+    if (ac[0] == bc[0] && ac[0] == '-')
+        return -strcmp(ac, bc);
+    
+    return strcmp(ac, bc);
+}
+
+char* read_all(FILE* file) {
+    size_t read_size;
+    long string_size;
+
+    char* buffer = NULL;
+    
+    fseek(file, 0, SEEK_END);
+    string_size = ftell(file);
+    rewind(file);
+
+    buffer = (char*) malloc(sizeof(char) * (string_size + 1));
+    read_size = fread(buffer, sizeof(char), (size_t) string_size, file);
+
+    buffer[string_size] = '\0';
+
+    if (string_size != read_size) {
+        free(buffer);
+        buffer = NULL;
+    }
+
+    fclose(file);
+
+    return buffer;
 }
 
 void read_numbers_from_file(const char* path) {
-    char buffer[BUFFER_SIZE];
-    int fd = open(path, O_RDONLY, 0444);
+    FILE* file = fopen(path, "r");
 
-    if (fd < 0) {
+    if (file == NULL) {
         printf("Error while open '%s'. Skipping...\n", path);
         return;
     }
 
-    int count = read(fd, buffer, BUFFER_SIZE);
+    char* content = read_all(file);
 
-    while (count != 0) {
-        char* lim_pointer = buffer;
-        char* end_pointer = buffer + count;
+    if (content == NULL) {
+        printf("Error while reading '%s'. Skipping...\n", path);
+        return;
+    }
 
-        while (lim_pointer != end_pointer) {
-            if (!isdigit(lim_pointer[0])) {
-                lim_pointer++;
-                continue;
-            }
-            
-            push(strtol(lim_pointer, &end_pointer, 10));
-            lim_pointer = end_pointer;
-            end_pointer = buffer + count;
-        }
+    long length = strlen(content);
 
-        count = read(fd, buffer, BUFFER_SIZE);
+    char buffer[BUFFER_SIZE];
+
+    for (int i = 0; i < length; i++) {
+        if (!isdigit(content[i]) && content[i] != '-')
+            continue;
+        
+        while (content[i] == '-')
+            i++;
+        
+        int substring_len = 0;
+
+        while (isdigit(content[i + substring_len]))
+            substring_len++;
+        
+        printf("'%c' %d\n", content[i], substring_len);
+        push(substring(content, i, substring_len));
     }
 }
 
@@ -89,17 +151,17 @@ int main(int argc, char **argv) {
         read_numbers_from_file(argv[i]);
     
     long count = size();
-    long numbers[count];
+    char* strs[count];
     long index = 0;
 
     node_t * current = root;
 
     while (current != NULL) {
-        numbers[index++] = current->val;
+        strs[index++] = current->val;
         current = current->next;
     }
 
-    qsort(numbers, count, sizeof(long), cmpnums);
+    qsort(strs, count, sizeof(char*), cmp_str_as_number);
 
     int fdo = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 
@@ -108,11 +170,9 @@ int main(int argc, char **argv) {
         return 1;
     }
     
-    char s[10];
-    for (int i = 0; i < count; i++) {
-        sprintf(s, "%ld ", numbers[i]);
-        write(fdo, s, strlen(s));
-    }
+    printf("%ld\n", count);
+    for (int i = 0; i < count; i++)
+        write(fdo, concat(strs[i], " "), strlen(strs[i]) + 1);
 
     return 0;
 }
